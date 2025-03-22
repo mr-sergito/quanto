c3f616fb145d50f5d92e9e5e1e7922d6e1a483be
mr-sergito <sergioleesp@gmail.com>
2025-03-22 18:23:39 +0100

    feat: Initial api structure

---
+++ quanto-api/src/Quanto.Api/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace Quanto.Api;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPresentation(this IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        return services;
+    }
+}
---
+++ quanto-api/src/Quanto.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Quanto.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        ConfigurationManager configuration)
+    {
+        services.AddPersistence(configuration);
+
+        return services;
+    }
+
+    public static IServiceCollection AddPersistence(
+        this IServiceCollection services,
+        ConfigurationManager configuration)
+    {
+        return services;
+    }
+}
